d4a97a26b0ca871fd7b143c4e88ff4894f891357
yaya1705 <yaya0517k@gmail.com>
2022-08-24 04:28:05 +0000

    [Add] indexページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,45 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_paramas)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   def show
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_paramas)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
+  end
+
+  private
+
+  def book_paramas
+    params.require(:book).permit(:title,:body)
   end
 end
